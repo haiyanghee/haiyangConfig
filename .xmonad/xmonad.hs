79dc5f82cffa63ef1d9f863458f3faf113abdc95
Haiyang He <imhhy123@hotmail.com>
2021-12-16 14:21:47 -0700

    added switching workspace and toggle monitors with independent workspaces! took some effort and its not the best, but ok

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -28,7 +28,7 @@ import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
 import XMonad.Util.SpawnOnce
 
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.DynamicBars
+-- import XMonad.Hooks.DynamicBars
 import XMonad.Hooks.ManageDocks
 
 import System.IO
@@ -40,22 +40,33 @@ import XMonad.Util.EZConfig(additionalKeys)
 import Data.Maybe
 
 import XMonad.Layout.IndependentScreens
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.Loggers
+
+import qualified XMonad.Hooks.WorkspaceHistory as WH
+
+import XMonad.Prelude (find, findIndex, isJust, isNothing, liftM2)
+
+
+-- super super braindead & nice way to switch between workspaces holy
+import XMonad.Actions.CycleWS
 
 
 main :: IO ()
 main = do
 
     spawn "killall trayer"  -- kill current trayer on each restart
-    spawn ("sleep 2 && trayer --edge top --align right --widthtype request  --SetDockType true --SetPartialStrut true --expand true  --transparent true --alpha 0 -- --height 16")
+    -- spawn ("sleep 2 && trayer --edge top --align right --widthtype request  --SetDockType true --SetPartialStrut true --expand true  --transparent true --alpha 0 --height 16")
+    spawn ("sleep 2 && trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor primary --transparent true --alpha 0 --height 16")
 
 
-
-    -- xmproc0 <- spawnPipe ("xmobar -x 0 ~/.config/xmobar/" ++ colorScheme ++ "-xmobarrc")
-    -- xmproc0 <- spawnPipe ("xmobar -x 0 ~/.config/xmobar/dracula-xmobarrc")
     xmproc0 <- spawnPipe ("xmobar -x 0 ~/.config/xmobar/archXmobarrc")
     xmproc1 <- spawnPipe ("xmobar -x 1 ~/.config/xmobar/archXmobarrc")
 
-    xmonad $  docks $ def
+    -- xmonad $  withEasySB (mySBL <> mySBR) defToggleStrutsKey $ docks $ def
+    -- xmonad $  dynamicEasySBs barSpawner  $ def
+    xmonad $  ewmh $ docks $ def
         {
             terminal           = myTerminal
 
@@ -72,44 +83,54 @@ main = do
             , keys               = myKeys
 
             , workspaces         = myWorkspaces
-            , focusedBorderColor = "#f8f8f2"
-            , normalBorderColor = "#282a36"
+            , focusedBorderColor = myFocusedBorderColor
+            , normalBorderColor = myNormalBorderColor
 
 
-            , handleEventHook    = handleEventHook def <+> docksEventHook
+            , handleEventHook    = handleEventHook def 
+
+            -- , handleEventHook    = handleEventHook def <+> docks
 
             -- , logHook = dynamicLogWithPP xmobarPP
-            --             { ppOutput = hPutStrLn xmproc0
-            --             , ppTitle = xmobarColor "green" "" 
-            --             , ppVisible = xmobarColor "#FFFFFF" "" .clickable
+            --             { -- ppOutput = hPutStrLn xmproc0,
+            --              ppTitle = xmobarColor "green" "" 
+            --             , ppVisible = xmobarColor "#FFFFFF" ""
             --             }
-            , logHook = dynamicLogWithPP  $ xmobarPP
-              -- XMOBAR SETTINGS
-              { ppOutput = \x -> hPutStrLn xmproc0 x   -- xmobar on monitor 1
-                              >> hPutStrLn xmproc1 x
-                -- Current workspace
-              , ppCurrent = xmobarColor color06 "" . wrap
-                            ("<box type=Bottom width=2 mb=2 color=" ++ color06 ++ ">") "</box>"
-                -- Visible but not current workspace
-              , ppVisible = xmobarColor color06 "" . clickable
-                -- Hidden workspace
-              , ppHidden = xmobarColor color05 "" . wrap
-                           ("<box type=Top width=2 mt=2 color=" ++ color05 ++ ">") "</box>" . clickable
-                -- Hidden workspaces (no windows)
-              , ppHiddenNoWindows = xmobarColor color05 ""  . clickable
-                -- Title of active window
-              , ppTitle = xmobarColor color16 "" . shorten 60
-                -- Separator character
-              , ppSep =  "<fc=" ++ color09 ++ "> <fn=1>|</fn> </fc>"
-                -- Urgent workspace
-              , ppUrgent = xmobarColor color02 "" . wrap "!" "!"
-                -- Adding # of windows on current workspace to the bar
-              , ppExtras  = [windowCount]
-                -- order of things in xmobar
-              , ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex++[t]
-              }
+            -- , logHook = myXmobarLogHook xmproc0 0 <+> myXmobarLogHook xmproc1 1 
 
+            -- , logHook = let log screen handle = dynamicLogWithPP . marshallPP screen $ myPP $ handle
+            --           in log 0 xmproc0 >> log 1 xmproc1
 
+            -- , logHook = myLog 0 xmproc0 >> myLog 1 xmproc1
+
+            , logHook =  
+                         -- myXmobarLogHook 0 xmproc0 
+                         -- >>
+                         -- myXmobarLogHook 1 xmproc1
+
+                    (dynamicLogWithPP . marshallPP 0 $ xmobarPP
+                         -- (   dynamicLogWithPP  $ xmobarPP
+                         { ppOutput = hPutStrLn xmproc0 
+                          , ppTitle = \x -> "" -- will display title in ppExtras
+                          -- , ppTitle = logTitles formatFocused formatUnfocused
+                         , ppHidden = xmobarColor "white" "" . clickable 0
+                          -- , ppExtras = [logTitleOnScreen 0]
+                         , ppExtras = [logWhenActive 0 $ logTitles formatFocused formatUnfocused
+                                            --,W.screen . W.current . windowset
+
+                                        ]
+                         } )
+                    >>
+                    (
+                    dynamicLogWithPP . marshallPP 1 $ xmobarPP
+                         -- (   dynamicLogWithPP  $ xmobarPP
+                         { ppOutput = hPutStrLn xmproc1
+                          , ppTitle = \x -> "" -- will display title in ppExtras
+                          -- , ppTitle = logTitles formatFocused formatUnfocused
+                         , ppHidden = xmobarColor "white" "" . clickable 1
+                          -- , ppExtras = [logTitleOnScreen 0]
+                         , ppExtras = [logWhenActive 1 $ logTitles formatFocused formatUnfocused]
+                         } )
 
 
             -- key bindings
@@ -120,11 +141,38 @@ main = do
             -- , startupHook        = dynStatusBarStartup $ startupHook defaultConfig
 
         }
+    where
+    formatFocused   =  xmobarColor "green" "" . shorten 50 . xmobarStrip
+    -- formatUnfocused = wrap "(" ")" . xmobarColor "#bd93f9" "" . shorten 30 . xmobarStrip
+    formatUnfocused = \x -> ""
+
 
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Vncviewer" --> doFloat
     ]
+
+-- myLog screen handle = dynamicLogWithPP . marshallPP screen $ xmobarPP
+--          { ppOutput = hPutStrLn handle 
+--           , ppTitle = xmobarColor "green" "" 
+--          , ppVisible = xmobarColor "#FFFFFF" "" .clickable screen
+--          } 
+-- 
+
+myXmobarLogHook screenNum handle =
+       dynamicLogWithPP . marshallPP screenNum $ xmobarPP
+                         -- (   dynamicLogWithPP  $ xmobarPP
+                         { ppOutput = hPutStrLn handle 
+                          , ppTitle = \x -> "" -- will display title in ppExtras
+                          -- , ppTitle = logTitles formatFocused formatUnfocused
+                         , ppHidden = xmobarColor "white" "" . clickable screenNum
+                          -- , ppExtras = [logTitleOnScreen 0]
+                         , ppExtras = [logWhenActive screenNum $ logTitles formatFocused formatUnfocused]
+                         } 
+    where
+    formatFocused   =  xmobarColor "green" "" . shorten 50 . xmobarStrip
+    -- formatUnfocused = wrap "(" ")" . xmobarColor "#bd93f9" "" . shorten 30 . xmobarStrip
+    formatUnfocused = \x -> ""
 
 
 -- color scheme
@@ -183,57 +231,173 @@ myKeys conf@(XConfig {X.modMask = modMask}) =
     , ((modMask, xK_n), spawn myBrowser)         -- starts browser
 
     , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
+
+
+    -- switch between adjacent workspaces
+    , ((modMask .|. shiftMask, xK_l),  moveTo Next spacesOnCurrentScreen)
+    , ((modMask .|. shiftMask, xK_h),  moveTo Prev spacesOnCurrentScreen)
+    , ((modMask .|. controlMask .|. shiftMask, xK_l),  shiftTo Next spacesOnCurrentScreen)
+    , ((modMask .|. controlMask .|. shiftMask, xK_h),  shiftTo Prev spacesOnCurrentScreen)
+    -- toggle between previous workspace
+    -- TODO: right now it can toggle between different screens...
+    -- , ((modMask              , xK_Tab),     toggleWS)
+    , ((modMask              , xK_Tab),     mytoggleWS' )
     ]
     ++
-        [ ( (m .|. modMask, k), X.windows $ f i)
-        | (i, k) <- zip (X.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0, xK_minus, xK_equal])
-        -- , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-        , (f, m) <- 
-            [ (W.view, noModMask)
-            , (W.greedyView, controlMask)
-            , (W.shift, shiftMask)
-            ]
-        ]
-         -- [
-         -- -- workspaces are distinct by screen
-         --  ((m .|. mod4Mask, k), windows $ onCurrentScreen f i)
-         --       | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
-         --       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
-         -- ]
+        -- [ ( (m .|. modMask, k), X.windows $ f i)
+        -- | (i, k) <- zip (X.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0, xK_minus, xK_equal])
+        -- -- , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+        -- , (f, m) <- 
+        --     [ (W.view, noModMask)
+        --     , (W.greedyView, controlMask)
+        --     , (W.shift, shiftMask)
+        --     ]
+        -- ]
+          [
+          -- workspaces are distinct by screen
+           ((m .|. modMask, k), windows $ onCurrentScreen f i)
+                | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
+                , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+          ]
+          ++
+          [((m .|. modMask, key), sc >>= screenWorkspace >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e] [(screenBy (-1)),(screenBy 1)]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
          -- ++
          -- [
          -- -- swap screen order
-         -- ((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-         --       | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
-         --       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+         --  ((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+         --        | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
+         --        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
                       
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = map show [1 .. 9 :: Int]
--- myWorkspaces = withScreens 2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+-- myWorkspaces = map show [1 .. 9 :: Int]
+myWorkspaces = withScreens 2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-myNormalBorderColor=undefined
-myFocusedBorderColor=undefined
+myFocusedBorderColor="#22d661"
+myNormalBorderColor="#444444"
                       
                       
-myMouseBindings=undefined
                       
                       
 myLayoutHook = smartBorders $ myDefaultLayout
              where
                myDefaultLayout =    Tall 1 (3/100) (1/2) ||| Full
 
-myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..] -- (,) == \x y -> (x,y)
+-- myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..] -- (,) == \x y -> (x,y)
+myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces ([1..9] ++ [1..9]) -- (,) == \x y -> (x,y)
 
 
-clickable ws = "<action=xdotool key super+"++show i++">"++ws++"</action>"
-    where i = fromJust $ M.lookup ws myWorkspaceIndices
+clickable monitorIndex ws = "<action=xdotool key super+"++show i ++">"++ws++"</action>"
+-- clickable monitorIndex ws = workStr
+    where 
+    workStr = show monitorIndex ++"_"++ ws
+    i = fromJust $ M.lookup workStr myWorkspaceIndices
 
-windowCount :: X (Maybe String)
-windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+-- clickable ws = "<action=xdotool key super+"++ show i++">"++ws++"</action>"
+--     where i = fromJust $ M.lookup ws myWorkspaceIndices
+
+-- windowCount :: X (Maybe String)
+-- windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
+
+
+isOnScreen :: ScreenId -> WindowSpace -> Bool
+isOnScreen s ws = s == unmarshallS (W.tag ws)
+
+currentScreen :: X ScreenId
+currentScreen = gets (W.screen . W.current . windowset)
+
+spacesOnCurrentScreen :: WSType 
+spacesOnCurrentScreen = 
+    WSIs $ do
+           s <- currentScreen
+           return  $ isOnScreen s
+
+genIndepTag i = map (\x -> str ++ show x) [1..9]
+    where
+    str = show i ++ "_"
+
+
+{--
+mylastViewedHiddenExcept :: X (Maybe WorkspaceId)
+mylastViewedHiddenExcept= do
+    curS <- currentScreen
+    let wdfS = if curS == 0 then 1 else 0
+    hs <- gets $ map W.tag . flip skipTags (genIndepTag wdfS). W.hidden . windowset
+    choose hs . find (`elem` hs) <$> WH.workspaceHistory
+    where choose []    _           = Nothing
+          choose (h:_) Nothing     = Just h
+          -- choose (h:_) Nothing     = Nothing
+          choose _     vh@(Just _) = vh
+--}
+
+{--
+mylastViewedHiddenExcept :: X (Maybe WorkspaceId)
+mylastViewedHiddenExcept = do
+    curS <- currentScreen
+    hs <- gets $ map W.tag . flip skipTags [] . W.hidden . windowset
+    -- hs <- gets $ map W.tag . flip skipTags (genIndepTag curS) . W.hidden . windowset
+    -- choose hs . find (\(sid,a) -> if curS == sid then a `elem` hs else False) <$> WH.workspaceHistoryWithScreen 
+    choose hs . find (\(sid,a) ->  a `elem` hs ) <$>   (\l ->  filter (\(sid,a) -> curS == sid) l)  <$> WH.workspaceHistoryWithScreen 
+        where 
+            choose []    _           = Nothing
+            choose (h:_) Nothing     = Just h
+            choose _     (Just (_,h)) = Just h
+--}
+
+-- {--
+mylastViewedHiddenExcept :: X (Maybe WorkspaceId)
+mylastViewedHiddenExcept = do
+    curS <- currentScreen
+    hs <- gets $ map W.tag . W.hidden . windowset
+    -- hs <- gets $ map W.tag . flip skipTags (genIndepTag curS) . W.hidden . windowset
+    choose curS hs . find (`elem` hs ) <$>   f curS WH.workspaceHistoryByScreen 
+    -- f curS WH.workspaceHistoryByScreen 
+        where 
+            f :: ScreenId -> X [(ScreenId, [WorkspaceId])]  ->X [WorkspaceId]
+            -- f gets the workspace Ids of the current screen
+            f curS wdfl = 
+                (\l ->  foldr (\(sid,l) acc -> if sid==curS then l else acc ) [] l) <$> wdfl
+            choose _ []    _           = Nothing
+            -- find the first physical (not virtual) workspace that is on the current screen!
+            choose curS l Nothing = 
+                foldl g Nothing l
+                where
+                g acc@(Just a) _ = acc
+                g Nothing e = 
+                        let (sid, e') = unmarshall e in
+                        if sid == curS then Just e else Nothing
+            choose _ _  (Just h) = Just h
+--}
+
+
+mytoggleWS' ::  X ()
+mytoggleWS' = mylastViewedHiddenExcept >>= flip whenJust (windows . W.view)
+
+-- getToggleWSList = M.map f myWorkspaceIndices
+--     where
+--     sid = do 
+--             s <- currentScreen 
+--             return s
+--     workStr = show sid ++"_"
+--     i = fromJust $ M.lookup workStr myWorkspaceIndices
+--     f = undefined
+
+
+
+
+
+
+
+
+
+
 
 -- myManageHook=undefined
 myEventHook=undefined
 myLogHook=undefined
 myStartupHook=undefined
+myMouseBindings=undefined
 
