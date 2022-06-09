7f0dfcbf9febb2c5b2c987bc19b604a3460ae83f
Haiyang He <imhhy123@hotmail.com>
2022-06-09 09:45:54 -0600

    small script changes

---
+++ scripts/one-screen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off
+
+sudo ~/.scripts/keyboard.sh
